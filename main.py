d0a5b8de39df0a75fe55b58b62ea449d829d8092
Aspangler4804 <aspangler1@ufl.edu>
2024-04-10 10:29:46 -0400

    second comit!!!!

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
 if __name__ == '__main__':
-    print('Hello World!')
+    def Encode():
+        EncodedPswrd = ""
+        for i in Pswrd:
+            update = int(i) + 3
+            if update < 10:
+                update = str(update)
+            else:
+                if update == 10:
+                    update = "1"
+                if update == 11:
+                    update = "2"
+                if update == 12:
+                    update = "3"
+            EncodedPswrd += (update)
+        print(EncodedPswrd)
 
+    while True:
+        Pswrd = (input("Please enter an 8 digit password:"))
+
+        try:
+            if len(Pswrd) == 8:
+                break
+            else:
+                print("Please enter a valid password")
+        except ValueError:
+            print("Please enter a valid size")
+    Encode(Pswrd)
 
